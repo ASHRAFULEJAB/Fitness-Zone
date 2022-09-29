c94e29392b27fdf850ec6e15b93a3a7454b0cd9e
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-09-29 23:13:24 +0600

    alert added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Details.css'
+import Swal from 'sweetalert2'
 
 const Details = ({times,breakTime}) => {
    
     let totalTime = 0;
     for(const excerise of times){
         totalTime = totalTime+ excerise.time
+
+    }
+    const btnClicked=()=>{
+        Swal.fire(
+            'Good job!',
+            'You clicked the button! You Have Finhed Your Break Time!!YAY!!',
+            'success'
+          )
     }
     return (
         <div className='mt-6'>
@@ -14,7 +23,8 @@ const Details = ({times,breakTime}) => {
                 <h2 className='border border-sky-500 rounded-lg bg-white text-3xl p-5 sm:w-5/12 lg:w-11/12'>Excerise Time:{totalTime}S</h2>
                 <h2 className='border border-sky-500 rounded-lg bg-white text-3xl p-5 mt-5 mb-5 sm:w-5/12 lg:w-11/12'>Break Time:{breakTime}</h2>
             </div>
-            <button  className=" place-content-center btn btn-secondary p-3 text-2xl text-white">Activity Completed</button>
+            <button  onClick={btnClicked} className=" place-content-center btn btn-secondary p-3 text-2xl text-white">Activity Completed </button>
+            
         </div>
     );
 };
