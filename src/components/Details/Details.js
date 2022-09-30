8aaad35842b70184c1f73478a1492f46f15170ca
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-09-30 07:00:23 +0600

    every-section-edited

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -12,7 +12,7 @@ const Details = ({times,breakTime}) => {
     const btnClicked=()=>{
         Swal.fire(
             'Good job!',
-            'You clicked the button! You Have Finhed Your Break Time!!YAY!!',
+            'YES HERO! You Have Finhed Your Break Time!!YAY!!',
             'success'
           )
     }
@@ -20,8 +20,8 @@ const Details = ({times,breakTime}) => {
         <div className='mt-6'>
             <h1  className='text-5xl mb-5'>Excerise Details</h1>
             <div>
-                <h2 className='overflow-hidden border border-sky-500 rounded-lg bg-white text-3xl p-5 sm:w-5/12 lg:w-96'>Excerise Time:{totalTime}S</h2>
-                <h2 className=' overflow-hidden border border-sky-500 rounded-lg bg-white text-3xl p-5 mt-5 mb-5 sm:w-5/12 lg:w-96'>Break Time:{breakTime}</h2>
+                <h2 className='overflow-hidden border border-sky-500 rounded-lg bg-white text-3xl p-5 sm:w-5/12 lg:w-96'>Excerise Time:{totalTime}Seconds</h2>
+                <h2 className=' overflow-hidden border border-sky-500 rounded-lg bg-white text-3xl p-5 mt-5 mb-5 sm:w-5/12 lg:w-96'>Break Time:{breakTime}Seconds</h2>
             </div>
             <button  onClick={btnClicked} className=" place-content-center btn btn-secondary p-3 text-2xl text-white">Activity Completed </button>
             
--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './PersonalInfo.css'
+import image from '../image/picc-1.png'
 
 const PersonalInfo = () => {
     return (
         <div >
-            <div className=" overflow-hidden card sm:w-5/12 2xl:w-96 lg:w-96 bg-purple-400 shadow-xl 2xl:grid grid-cols-2  lg: grid grid-cols-2">
-                  <figure><img  className='w-40 h-40 m-3 rounded-full' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div className=" overflow-hidden card sm:w-5/12 2xl:w-96 lg:w-96 bg-gray-400 shadow-xl 2xl:grid grid-cols-2  lg: grid grid-cols-2">
+                  <figure><img  className='m-3 rounded-full' src={image} alt="" /></figure>
              <div className="card-body">
                <h1 className="card-title 2xl:text-3xl lg:text-2xl">Ashraful Islam</h1>
                <p className='2xl:text-3xl lg:text-2xl'><b>Age:22</b></p>
-               <p className='2xl:text-3xl lg:text-2xl'><b>Height:5.922</b></p>
+               <p className='2xl:text-3xl lg:text-2xl'><b>Height:5.9</b></p>
                <p className='2xl:text-3xl lg:text-2xl'><b>Weight:60Kg</b></p>
              
           </div>
